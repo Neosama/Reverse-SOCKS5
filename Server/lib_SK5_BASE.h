ef858faca439dc9a4670c54b06cbc8efdca079d2
Neosama <Neosama@users.noreply.github.com>
2019-02-18 15:31:39 +0100

    Update lib_SK5_BASE.h

--- Server/lib_SK5_BASE.h
+++ Server/lib_SK5_BASE.h
@@ -1,9 +1,5 @@
 
-/*
-
-LIB SK5 - BASE
-
-*/
+// SERVER_SK5_V2
 
 std::string string_hex(std::string str, const bool capital = false)
 {
@@ -54,11 +50,11 @@ static BOOL StartServer(INT port, SOCKET *s, SOCKADDR_IN *addr, INT *addrSize)
 	if (listen(*s, SOMAXCONN) == SOCKET_ERROR)
 		return FALSE;
 
-	getsockname(*s, (SOCKADDR *)addr, addrSize);
 	return TRUE;
 }
 
-void set_fds(int sock1, int sock2, fd_set *fds) {
+void set_fds(int sock1, int sock2, fd_set *fds)
+{
 	FD_ZERO(fds);
 	FD_SET(sock1, fds);
 	FD_SET(sock2, fds);
@@ -85,7 +81,7 @@ unsigned long WINAPI Fonction1(void* params)
 	int timeout_sec = sub->timeout_sec;
 	int buf_size = sub->buf_size;
 
-	char buf[1024 * 10];
+	char buf[512]; // If changed, line 22 in main.cpp is a change too
 
 	fd_set readfds;
 	int result, nfds = max(s_CLIENT, s_SK5) + 1;
@@ -98,39 +94,54 @@ unsigned long WINAPI Fonction1(void* params)
 
 	memset(buf, 0, sizeof(buf));
 
-	while ((result = select(nfds, &readfds, 0, 0, &tv)) > 0)  // s_Client <-> s_SK5
+	while (TRUE)  // s_Client <-> s_SK5
 	{
-		if (FD_ISSET(s_CLIENT, &readfds)) // s_Client -> s_SK5
+		if ((result = select(nfds, &readfds, 0, 0, &tv)) > 0)
 		{
-			int recvd_A = recv(s_CLIENT, buf, buf_size, 0);
-			if (recvd_A <= 0)
+			if (FD_ISSET(s_CLIENT, &readfds)) // s_Client -> s_SK5
 			{
-				printf("[ERROR] s_Client -> s_SK5 recvd (%i) <= 0\n", recvd_A);
-				closesocket(s_CLIENT);
-				return 3;
+				int recvd_A = recv(s_CLIENT, buf, buf_size, 0);
+				if (recvd_A <= 0)
+				{
+					printf("[!] s_Client -> s_SK5 recvd (%i) <= 0\n", recvd_A);
+					closesocket(s_CLIENT);
+					return 3;
+				}
+
+				int sendd_A = send(s_SK5, buf, recvd_A, 0);
+				printf("SEND_A : %i\n", sendd_A);
 			}
 
-			int sendd_A = send(s_SK5, buf, recvd_A, 0);
-		}
-
-		if (FD_ISSET(s_SK5, &readfds))  // s_SK5-> s_Client
-		{
-			int recvd_B = 0;
-
-			recvd_B = recv(s_SK5, buf, buf_size, 0);
-
-			if (recvd_B <= 0)
+			if (FD_ISSET(s_SK5, &readfds))  // s_SK5-> s_Client
 			{
-				printf("[ERROR] s_SK5-> s_Client recvd (%i) <= 0\n", recvd_B);
-				closesocket(s_CLIENT);
-				return 4;
-			}
-			int sendd_B = send(s_CLIENT, buf, recvd_B, 0);
-		}
-		set_fds(s_CLIENT, s_SK5, &readfds);
+				int recvd_B = 0;
 
-		if (OLDMODEM_mode)
-			Sleep(250);
+				recvd_B = recv(s_SK5, buf, buf_size, 0);
+
+				if (recvd_B <= 0)
+				{
+					printf("[!] s_SK5-> s_Client recvd (%i) <= 0\n", recvd_B);
+					closesocket(s_CLIENT);
+					return 4;
+				}
+
+				int sendd_B = send(s_CLIENT, buf, recvd_B, 0);
+				printf("SEND_B : %i\n", sendd_B);
+			}
+			set_fds(s_CLIENT, s_SK5, &readfds);
+
+			if (OLDMODEM_mode)
+				Sleep(250);
+		}
+		else
+		{
+			printf("[!] select timeout\n");
+
+			shutdown(s_CLIENT, 2);
+			closesocket(s_CLIENT);
+			closesocket(s_SK5);
+			return 1;
+		}
 	}
 
 	shutdown(s_CLIENT, 2);
@@ -139,12 +150,13 @@ unsigned long WINAPI Fonction1(void* params)
 	return 0;
 }
 
-static int create_sock_thread(threaddata f)
+static int create_sock_thread(void* f)
 {
 	HANDLE handle;
-	if (!(handle = CreateThread(NULL, 0, Fonction1, (void *)&f, 0, 0)))
-		return 0;
-	CloseHandle(handle);
 
+	if (!(handle = CreateThread(NULL, 0, Fonction1, f, 0, 0)))
+		return 0;
+
+	CloseHandle(handle);
 	return 1;
 }
