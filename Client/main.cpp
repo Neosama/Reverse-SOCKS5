bc8101e1fa1ac7856940e0276866fbe239ee259b
Neosama <Neosama@users.noreply.github.com>
2019-02-18 15:30:17 +0100

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,4 +1,8 @@
 
+// CLIENT_SK5_V2
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -12,40 +16,48 @@
 
 int main()
 {
-	//FreeConsole();
-	std::cout << "SK5 V2 Client\n" << std::endl;
+	printf("CLIENT_SK5_V2\n");
 
-	// Variables Parameters
-	char *ip_SK5 = "127.0.0.1";
-    int port_SK5 = 40001;
-	BOOL VERBOSE_mode = TRUE;
-	int timeout_sec = 30;
-	int buf_size = 512; // MAX 1024 * 10
+	// Configuration variables
+	BOOL VERBOSE_mode = TRUE; // If FALSE only [!] is show
+	const char *ip_SK5 = "127.0.0.1";
+	int port_SK5 = 40001;
+	int buf_size = 512; // If changed, line 202 in lib_SK5.h is a change too
+	int timeout_sec = 30; // seconds
+	int max_connect_failed = 1; // 1 for infinite
 
 	// Initiates Winsock
 	WSADATA WSAData;
 	int ret = WSAStartup(MAKEWORD(2, 0), &WSAData);
 	if (ret != 0)
 	{
-		printf("[ERROR] WSAStartup error: %i\n", ret);
+		printf("[!] WSAStartup error: %i\n", ret);
 		return -1;
 	}
 	else
 		if (VERBOSE_mode) printf("[+] WSAStartup success\n");
-	
-	while(1)
-	{
-		SOCKET sock;    
-		SOCKADDR_IN sin;
-		sin.sin_family=AF_INET;
-		sin.sin_addr.s_addr=inet_addr(ip_SK5);
-		sin.sin_port=htons(port_SK5);
-		sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-		if (sock == INVALID_SOCKET)
-			return -1;
 
-		if(connect(sock,(SOCKADDR*)&sin,sizeof(sin)) != SOCKET_ERROR) // Connect to SK5_BASE
+	int connect_failed = 0;
+
+	while (TRUE)
+	{
+		SOCKET sock;
+		SOCKADDR_IN sin;
+		sin.sin_family = AF_INET;
+		sin.sin_addr.s_addr = inet_addr(ip_SK5);
+		sin.sin_port = htons(port_SK5);
+		sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (sock == INVALID_SOCKET)
 		{
+			printf("[!] sock INVALID_SOCKET\n");
+			return -1;
+		}
+
+		if (connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR)
+		{
+			if (VERBOSE_mode) printf("[+] Connect to SK5 success\n");
+			connect_failed = 0;
+
 			fd_set readfds;
 			int result, nfds = sock + 1;
 			FD_ZERO(&readfds);
@@ -61,16 +73,20 @@ int main()
 					qq.timeout_sec = timeout_sec;
 					qq.buf_size = buf_size;
 
-					if (VERBOSE_mode) printf("[+] Connect to SK5_BASE success\n");
-					create_sock_thread(qq);
+					create_sock_thread((void*)&qq);
 				}
 			}
 		}
 		else
-			printf("[ERROR] Connect to SK5_BASE error\n", ret);
+		{
+			connect_failed++;
+			printf("[!] Connect to SK5 (%i) error\n", connect_failed);
+		}
+
+		if (connect_failed >= max_connect_failed && max_connect_failed != 1)
+			return -2;
 	}
 
-	system("\n\npause");
+	getchar();
 	return 0;
 }
-
